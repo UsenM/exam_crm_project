d90cc7eeefe15eb34f5c4ef20b626904168713fe
makhabattk <makhabat.tk@gmail.com>
2022-04-18 18:18:17 +0600

    duplicated Dao classes deleted

--- src/main/java/dao/impl/CourseDao.java
+++
@@ -1,7 +0,0 @@
-package dao.impl;
-
-import dao.CrudDao;
-import entities.Course;
-
-public interface CourseDao extends CrudDao<Course> {
-}
--- src/main/java/dao/impl/CourseFormatDao.java
+++
@@ -1,7 +0,0 @@
-package dao.impl;
-
-import dao.CrudDao;
-import entities.CourseFormat;
-
-public interface CourseFormatDao extends CrudDao<CourseFormat> {
-}
